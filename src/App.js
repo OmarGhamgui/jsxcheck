80c1a0badb449bfca7bda6cf0cc7857f0f7d43ae
OmarGhamgui <70692545+OmarGhamgui@users.noreply.github.com>
2020-11-01 15:46:16 +0100

    v1

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+import image2 from "./imageInSrc.jpg";
+function App() {
+  return (
+    <div className="App">
+      <div style={{ border: "10px solid" }}>
+        <h1 class="title red">Omar Ghamgui</h1>
+
+        <br />
+
+        <img src={image2} />
+
+        <br />
+
+        <img src="/imageInPublic.jpg" />
+      </div>
+
+      <video width="320" height="240" controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
+}
+
+export default App;
